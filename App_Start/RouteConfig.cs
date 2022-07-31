633eb6a41036a7d1a9b765d749dcdd3967b13d23
hzd1719 <87230980+hzd1719@users.noreply.github.com>
2022-07-31 22:05:41 +0300

    Add files via upload

---
+++ App_Start/RouteConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace TheForum
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            /*routes.IgnoreRoute("");*/
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                //defaults: new { controller = "TheForum", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "TheForum", action = "Index", id = UrlParameter.Optional}
+
+
+            );
+        }
+    }
+}
