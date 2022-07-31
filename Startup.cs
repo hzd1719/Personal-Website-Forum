5cbf7c675235cf7fb174eb04fd6808ee82979c1b
hzd1719 <87230980+hzd1719@users.noreply.github.com>
2022-07-31 22:10:43 +0300

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TheForum.Startup))]
+namespace TheForum
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
